7c2f194ecf77d9cd0b67b9e3254ea4e626d77b88
Rehan JHUBOO <rehan.jhuboo@univ-st-etienne.fr>
2023-04-20 10:22:24 +0200

    Workin' Unet

--- Train.py
+++ Train.py
@@ -10,6 +10,8 @@ import torch.nn as nn
 from sklearn.model_selection import train_test_split
 import torch.optim as optim
 import dataloader
+import time
+from torchvision.utils import save_image
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--label_dir", default = "/gpfsstore/rech/tvs/uki75tv/Train_LR_segmented", help = "path to label csv file")
@@ -17,18 +19,37 @@ parser.add_argument("--image_dir", default = "/gpfsstore/rech/tvs/uki75tv/Train_
 parser.add_argument("--batch_size", default = 16, help = "Batch size")
 parser.add_argument("--nb_epochs", default = 100, help = "epochs")
 parser.add_argument("--nb_workers", default = 4, help = "worker")
-parser.add_argument("--lr",default=1e-3,help="learning rate")
+parser.add_argument("--lr",default=5e-3,help="learning rate")
 opt = parser.parse_args()
 
 NB_DATA = 7100
+if torch.cuda.device_count() > 1:
+    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+else:
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 index = range(NB_DATA)
-split = train_test_split(index,test_size=1000,shuffle=False)
+index = []
+
+start = 49
+end = 99
+
+while start <= 7099:
+    for i in range(start, end+1):
+        index.append(i)
+    start += 100
+    end += 100
+split = train_test_split(index,test_size=500,shuffle=False)
 datasets = dataloader.Datasets(opt.image_dir,opt.label_dir,opt) # Create dataset
 trainloader = DataLoader(datasets, batch_size = opt.batch_size, sampler = split[0], num_workers = opt.nb_workers )
 testloader =DataLoader(datasets, batch_size = 1, sampler = split[1], num_workers = opt.nb_workers )
 
 criterion = nn.BCEWithLogitsLoss()
 model = Model.UNet()
+if torch.cuda.device_count() > 1:
+    model = nn.DataParallel(model)
+model.to(device)
+
 # Assuming your U-Net model is called `model`
 optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
@@ -43,24 +64,33 @@ def iou_metric(outputs, targets, threshold=0.5):
     return iou
 
 test_metric = []
-
+train_loss = []
 for epoch in range(opt.nb_epochs):
+    start_time= time.time()
     for i, data in enumerate(trainloader,0):
-        labels, inputs = data['label'],data['image']
+        labels, inputs = data['label'].to(device),data['image'].to(device)
         inputs = inputs.reshape(inputs.size(0),1,512,512)
         labels = labels.reshape(labels.size(0),1,512,512)
         outputs = model(inputs)
         loss = criterion(outputs, labels)
-
+        optimizer.zero_grad()
+        train_loss.append(loss.item())
         # Backpropagation and optimizer step
         loss.backward()
         optimizer.step()
+        if i % 100 == 0:
+            print("Running Loss: {:4f}".format(loss.item()))
+    end_time = time.time()
+    total_time = end_time - start_time
+    print("Epoch {}, in {:2f}: Loss={:4f}".format(epoch,total_time,np.mean(train_loss)))
+    
     with torch.no_grad():
         for i, data in enumerate(testloader,0):
-            labels,inputs = data['label'],data['image']
+            labels,inputs,imagename = data['label'].to(device),data['image'].to(device),data['name']
             inputs = inputs.reshape(inputs.size(0),1,512,512)
             labels = labels.reshape(labels.size(0),1,512,512)
             outputs=model(inputs)
             metric = iou_metric(outputs,labels)
-            test_metric.append(metric)
-        print("Epoch",epoch,": Metric=",mean(test_metric))
+            test_metric.append(metric.cpu())
+            save_image(outputs[0],"./output2/"+imagename[0].replace(".png","_output.png"))
+        print("Epoch",epoch,": Metric=",np.mean(test_metric))
--- dataloader.py
+++ dataloader.py
@@ -4,11 +4,16 @@ import numpy as np
 from skimage import io
 from skimage.color import rgb2gray
 from torch.utils.data import Dataset, DataLoader
+from skimage import transform
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+import random
+
 
 class Datasets(Dataset):
     def __init__(self,image_dir, label_dir, opt):
         self.opt = opt
-        self.imagename = os.listdir(image_dir)
+        self.imagename = sorted(os.listdir(image_dir))
         self.image_dir = image_dir
         self.label_dir = label_dir
         self.upsample=False
@@ -18,18 +23,31 @@ class Datasets(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        
+        print(self.imagename)
         image = io.imread(os.path.join(self.image_dir,self.imagename[idx])) # Loading Image
         label = io.imread(os.path.join(self.label_dir,self.imagename[idx]))
         if self.upsample == True:
             image = transform.rescale(image,2)
             image = (image>0.5)*255
             mask_name = os.path.join(self.mask_dir,str(self.labels.iloc[idx,0]).replace("_lr.tif",".bmp"))
-        image = rgb2gray(image)
-        label = rgb2gray(label)
+        
         label = label / 255.0
         label = label.astype('float32')
         image = image / 255.0 # Normalizing [0;1]
-        image = image.astype('float32') # Converting images to float32 
+        image = image.astype('float32') # Converting images to float32
+
+        p = random.random()
+        rot = random.randint(-45,45)
+        image,label=TF.to_pil_image(image),TF.to_pil_image(label)
+        image,label=TF.rotate(image,rot),TF.rotate(label,rot)
+        if p<0.3:
+            image,label=TF.vflip(image),TF.vflip(label)
+        p = random.random()
+        if p<0.3:
+            image,label=TF.hflip(image),TF.hflip(label)
+        p = random.random()
+        if p>0.2:
+            image,label=TF.affine(image,angle=0,translate=(0.1,0.1),shear=0,scale=1),TF.affine(label,angle=0,translate=(0.1,0.1),shear=0,scale=1)
+        image,label=TF.to_tensor(image),TF.to_tensor(label)
         
-        return {'image': image, 'label': label}
+        return {'image': image, 'label': label, 'name': self.imagename[idx]}
